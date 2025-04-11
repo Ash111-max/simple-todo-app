2b85a92947a8266e2d74c0cf2ffcb9113c58222a
Ash111-max <dsilvaastle111@gmail.com>
2025-04-12 00:54:01 +0530

    created a simple todo app using react

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// src/App.jsx
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import './styles.css';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} removeTodo={removeTodo} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+// src/components/TodoList.jsx
+import React from 'react';
+
+const TodoList = ({ todos, removeTodo }) => {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>
+          {todo}
+          <button onClick={() => removeTodo(index)}>Remove</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
